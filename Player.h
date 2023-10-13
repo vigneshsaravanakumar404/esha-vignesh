5c34094cfa66d0e3bc17c22b6807b724d8016b88
eshavignesh123 <94078775+eshavignesh123@users.noreply.github.com>
2023-10-13 10:35:00 -0400

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,1 +1,26 @@
+#pragma 
 
+#include "raylib.h"
+#include "raymath.h"
+
+class Vehicle
+{
+public:
+    Vector2 location;
+    Vector2 velocity;
+    Vector2 acceleration;
+    Vector2 desiredLocation;
+    float r;
+    float maxforce;
+    float maxspeed;
+    Vector2 steer;
+
+    Vehicle();
+
+    Vehicle(float x, float y);
+
+    void update();
+    void applyForce(Vector2 force);
+    void arrive(Vector2 target);
+    float map(float value, float fromLow, float fromHigh, float toLow, float toHigh);
+};
