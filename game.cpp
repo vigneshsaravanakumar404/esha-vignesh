76afcc970312e6a52ada19bffd1e0decb43303af
eshavignesh123 <94078775+eshavignesh123@users.noreply.github.com>
2023-10-17 12:04:35 -0400

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -54,24 +54,28 @@ int main(void)
 
     }
 
-
-    Vehicle wandererOne = Vehicle(rand() % 790, rand() % 440);
-    Vehicle wandererTwo = Vehicle(rand() % 790, rand() % 440);
-    Vehicle wandererThree = Vehicle(rand() % 790, rand() % 440);
-    Vehicle wandererFour = Vehicle(rand() % 790, rand() % 440);
-    Vehicle wandererFive = Vehicle(rand() % 790, rand() % 440);
-
-
-
-
+    Vehicle wanderers[5];
+    for (int i = 0; i < 5; i++)
+    {
+        wanderers[i]= Vehicle(rand() % 790, rand() % 440);
+    }
 
     bool updateRandomCircles = true;
     clock_t lastUpdateTime = clock();
     float randomCircleInterval = 30.0f; // Change random circle positions every 5 seconds
 
     bool updatePosition = true;
-    clock_t lastUpdateTimePos = clock();
-    float randomPosInterval = 3.0f; // Change position  positions every 5 seconds
+    clock_t lastUpdateTimePosOne = clock();
+    clock_t lastUpdateTimePosTwo = clock();
+    clock_t lastUpdateTimePosThree = clock();
+    clock_t lastUpdateTimePosFour = clock();
+    clock_t lastUpdateTimePosFive = clock();
+
+    float randomPosIntervalOne = 3.0f; // Change position  positions every 5 seconds
+    float randomPosIntervalTwo = 5.0f;
+    float randomPosIntervalThree = 2.0f;
+    float randomPosIntervalFour = 4.0f;
+    float randomPosIntervalFive = 6.0f;
 
     Texture2D ship = LoadTexture("ships.png"); 
     ship.width *= 0.1;
@@ -110,23 +114,46 @@ int main(void)
         if (updatePosition)
         {
             clock_t currentTime = clock();
-            float elapsedSeconds = (currentTime - lastUpdateTimePos) / CLOCKS_PER_SEC;
+            float elapsedSecondsOne = (currentTime - lastUpdateTimePosOne) / CLOCKS_PER_SEC;
+            float elapsedSecondsTwo = (currentTime - lastUpdateTimePosTwo) / CLOCKS_PER_SEC;
+            float elapsedSecondsThree = (currentTime - lastUpdateTimePosThree) / CLOCKS_PER_SEC;
+            float elapsedSecondsFour = (currentTime - lastUpdateTimePosFour) / CLOCKS_PER_SEC;
+            float elapsedSecondsFive = (currentTime - lastUpdateTimePosFive) / CLOCKS_PER_SEC;
 
-            if (elapsedSeconds >= randomPosInterval)
+
+            if (elapsedSecondsOne >= randomPosIntervalOne)
             {
-                wandererOne.desiredLocation.x = rand() % 790;
-                wandererOne.desiredLocation.y = rand() % 440; 
-                wandererTwo.desiredLocation.x = rand() % 790;
-                wandererTwo.desiredLocation.y = rand() % 440;
-                wandererThree.desiredLocation.x = rand() % 790;
-                wandererThree.desiredLocation.y = rand() % 440;
-                wandererFour.desiredLocation.x = rand() % 790;
-                wandererFour.desiredLocation.y = rand() % 440;
-                wandererFive.desiredLocation.x = rand() % 790;
-                wandererFive.desiredLocation.y = rand() % 440;
+                wanderers[0].desiredLocation.x = rand() % 790;
+                wanderers[0].desiredLocation.y = rand() % 440;
+                lastUpdateTimePosOne = currentTime;
+            }
 
-                lastUpdateTimePos = currentTime;
+            if (elapsedSecondsTwo >= randomPosIntervalTwo)
+            {
+                wanderers[1].desiredLocation.x = rand() % 790;
+                wanderers[1].desiredLocation.y = rand() % 440;
+                lastUpdateTimePosTwo = currentTime;
+            }
+            if (elapsedSecondsThree >= randomPosIntervalThree)
+            {
+                wanderers[2].desiredLocation.x = rand() % 790;
+                wanderers[2].desiredLocation.y = rand() % 440;
+                lastUpdateTimePosThree = currentTime;
 
+            }
+            if (elapsedSecondsFour >= randomPosIntervalFour)
+            {
+               
+                wanderers[3].desiredLocation.x = rand() % 790;
+                wanderers[3].desiredLocation.y = rand() % 440;
+                lastUpdateTimePosFour = currentTime;
+
+            }
+            if (elapsedSecondsFive >= randomPosIntervalFive)
+            {
+                wanderers[4].desiredLocation.x = rand() % 790;
+                wanderers[4].desiredLocation.y = rand() % 440;
+                lastUpdateTimePosFive = currentTime;
             }
 
         }
@@ -137,43 +164,51 @@ int main(void)
             DrawCircle(foods[i].xPos, foods[i].yPos, 5.0f, foods[i].color);
         }
 
-        wandererOne.arrive(wandererOne.desiredLocation);
-        wandererOne.update();
-        wandererTwo.arrive(wandererTwo.desiredLocation);
-        wandererTwo.update();
-        wandererThree.arrive(wandererThree.desiredLocation);
-        wandererThree.update();
-        wandererFour.arrive(wandererFour.desiredLocation);
-        wandererFour.update();
-        wandererFive.arrive(wandererFive.desiredLocation);
-        wandererFive.update();
+        /*
+        for (int i = 0; i < 5; i++)
+        {
+            wanderers[i].arrive(wanderers[i].desiredLocation);
+            wanderers[i]
+        }
+        */
 
-        float angleOne = atan2f(wandererOne.desiredLocation.y - wandererOne.location.y, wandererOne.desiredLocation.x - wandererOne.location.x);
+        wanderers[0].arrive(wanderers[0].desiredLocation);
+        wanderers[0].update();
+        wanderers[1].arrive(wanderers[1].desiredLocation);
+        wanderers[1].update();
+        wanderers[2].arrive(wanderers[2].desiredLocation);
+        wanderers[2].update();
+        wanderers[3].arrive(wanderers[3].desiredLocation);
+        wanderers[3].update();
+        wanderers[4].arrive(wanderers[4].desiredLocation);
+        wanderers[4].update();
+
+        float angleOne = atan2f(wanderers[0].desiredLocation.y - wanderers[0].location.y, wanderers[0].desiredLocation.x - wanderers[0].location.x);
         float angleDiffOne = angleOne - rotationOne;
         rotationOne += angleDiffOne * 0.05f; 
 
-        float angleTwo = atan2f(wandererTwo.desiredLocation.y - wandererTwo.location.y, wandererTwo.desiredLocation.x - wandererTwo.location.x);
+        float angleTwo = atan2f(wanderers[1].desiredLocation.y - wanderers[1].location.y, wanderers[1].desiredLocation.x - wanderers[1].location.x);
         float angleDiffTwo = angleTwo - rotationTwo;
         rotationTwo += angleDiffTwo * 0.05f;
 
-        float angleThree = atan2f(wandererThree.desiredLocation.y - wandererThree.location.y, wandererThree.desiredLocation.x - wandererThree.location.x);
+        float angleThree = atan2f(wanderers[2].desiredLocation.y - wanderers[2].location.y, wanderers[2].desiredLocation.x - wanderers[2].location.x);
         float angleDiffThree = angleThree - rotationThree;
         rotationThree += angleDiffThree * 0.05f;
 
-        float angleFour = atan2f(wandererFour.desiredLocation.y - wandererFour.location.y, wandererFour.desiredLocation.x - wandererFour.location.x);
+        float angleFour = atan2f(wanderers[3].desiredLocation.y - wanderers[3].location.y, wanderers[3].desiredLocation.x - wanderers[3].location.x);
         float angleDiffFour = angleFour - rotationFour;
         rotationFour += angleDiffFour * 0.05f;
 
-        float angleFive = atan2f(wandererFive.desiredLocation.y - wandererFive.location.y, wandererFive.desiredLocation.x - wandererFive.location.x);
+        float angleFive = atan2f(wanderers[4].desiredLocation.y - wanderers[4].location.y, wanderers[4].desiredLocation.x - wanderers[4].location.x);
         float angleDiffFive = angleFive - rotationFive;
         rotationFive += angleDiffFive * 0.05f;
       
         Rectangle shipRectangle = { 0,0, ship.width, ship.height };
-        Rectangle newRectangleOne = { wandererOne.location.x,wandererOne.location.y, ship.width, ship.height };
-        Rectangle newRectangleTwo = { wandererTwo.location.x,wandererTwo.location.y, ship.width, ship.height };
-        Rectangle newRectangleThree = { wandererThree.location.x,wandererThree.location.y, ship.width, ship.height };
-        Rectangle newRectangleFour = { wandererFour.location.x,wandererFour.location.y, ship.width, ship.height };
-        Rectangle newRectangleFive = { wandererFive.location.x,wandererFive.location.y, ship.width, ship.height };
+        Rectangle newRectangleOne = { wanderers[0].location.x,wanderers[0].location.y, ship.width, ship.height };
+        Rectangle newRectangleTwo = { wanderers[1].location.x,wanderers[1].location.y, ship.width, ship.height };
+        Rectangle newRectangleThree = { wanderers[2].location.x,wanderers[2].location.y, ship.width, ship.height };
+        Rectangle newRectangleFour = { wanderers[3].location.x,wanderers[3].location.y, ship.width, ship.height };
+        Rectangle newRectangleFive = { wanderers[4].location.x,wanderers[4].location.y, ship.width, ship.height };
         Vector2 shipCenter = { ship.width / 2, ship.height / 2 };
 
         DrawTexturePro(ship, shipRectangle, newRectangleOne, shipCenter, rotationOne * RAD2DEG + 90, WHITE);
