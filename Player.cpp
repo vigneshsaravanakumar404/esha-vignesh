3edc21e890bedb3199bbba77f21bd91a5750ead9
Vignesh Saravanakumar <74271045+vigneshsaravanakumar404@users.noreply.github.com>
2023-10-19 15:47:42 -0400

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -32,24 +32,43 @@ void Vehicle::applyForce(Vector2 force)
 	acceleration = Vector2Add(force, acceleration);
 }
 
-void Vehicle::arrive(Vector2 target) {
-	Vector2 desired = Vector2Subtract(target, location);
-	float d = sqrt((desired.x) * (desired.x) + (desired.y) * (desired.y));
-	desired = Vector2Normalize(desired);
-	if (d < 100) {
-		float m = map(d, 0, 100, 0, maxspeed);
-		desired = Vector2Scale(desired, m);
-	}
-	else
-	{
-		desired = Vector2Scale(desired, maxspeed);
+void Vehicle::arrive(Vector2 target, float deltaTime) {
+    const float scaleFactor = 0.16f, maxPlayerVelocity = 3.0f, slowingRadius = 115.0f, maxSpeed = 3.5f, maxForce = 4.5f, maxSteeringRate = PI / 10.5f, stopRadius = 1.0f, timeToWait = 6.0;
+    float deltaTime1 = deltaTime;
 
-	}
+    Vector2 desired = Vector2Subtract(target, location);
+    float d = Vector2Length(desired);  // Calculate the distance
 
-	Vector2 steer = Vector2Subtract(desired, velocity);
-	Vector2ClampValue(steer, 0.0f, maxforce);
-	applyForce(steer);
+    if (d > 0) {
+        // Normalize the desired velocity vector and scale it to maxSpeed
+        desired = Vector2Normalize(desired);
+        if (d < stopRadius) {
+            // If within the stop radius, bring the vehicle to a stop
+            desired = Vector2{ 0.0f, 0.0f };
+        }
+        else if (d < slowingRadius) {
+            // Within the slowing radius, map the speed between 0 and maxSpeed
+            float m = map(d, 0, slowingRadius, 0, maxspeed);
+            desired = Vector2Scale(desired, m);
+        }
+        else {
+            desired = Vector2Scale(desired, maxspeed);
+        }
+
+        // Calculate the steering force and clamp it
+        Vector2 steer = Vector2Subtract(desired, velocity);
+        Vector2ClampValue(steer, 0.0f, maxSteeringRate);
+
+        // Apply the force considering the time step (deltaTime)
+        steer = Vector2Scale(steer, deltaTime1);
+        applyForce(steer);
+
+        // Update the velocity considering the time step (deltaTime)
+        velocity = Vector2Add(velocity, Vector2Scale(steer, deltaTime1));
+        Vector2ClampValue(velocity, 0.0f, maxspeed);
+    }
 }
+
 
 float Vehicle::map(float value, float fromLow, float fromHigh, float toLow, float toHigh) {
 	return (value - fromLow) / (fromHigh - fromLow) * (toHigh - toLow) + toLow;
