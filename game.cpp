b652341e73d23fa210202b2e0d96febde4739ffe
eshavignesh123 <94078775+eshavignesh123@users.noreply.github.com>
2023-10-18 21:11:16 -0400

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -12,6 +12,7 @@ using namespace std;
 // TODO: Movement even when there is no food in 
 // TODO: Create visual for cross mutation for change in blue speed
 // TODO: Center red triangle after vision range increases
+
 // Structs
 typedef struct Food {
 public:
